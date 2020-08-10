f1f96571e05673335881f1c9105dfc9630816b79
Muhammad Arslan <mar@axew.io>
2020-08-10 15:01:11 +0500

    Second commit

---
+++ ConsumerDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace ConsumerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "gid-consumers",
+                BootstrapServers = "localhost:9092"
+            };
+            using (var consumer= new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("chat-message");
+                while (true)
+                {
+                    var cr = consumer.Consume();
+                    Console.WriteLine(cr.Message.Value);
+                }
+            }
+        }
+    }
+}
